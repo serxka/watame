507da2aae4b0f28e39a8f81bfc44df018a23ee04
serxka <serxka@protonmail.com>
2021-11-13 21:13:29 +1100

    Updated docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,7 @@
 # The idea is, if the storage folder holds an image folder
 # we have likely already run, another file would be fine,
 # I just find that a bit messy :P (though this is stupid)
-if [ ! -f "/storage/img" ]; then
+if [ ! -d "/storage/img" ]; then
 	watame --action create-folders
 	watame --action install-schema
 	watame --action clear-sessions
